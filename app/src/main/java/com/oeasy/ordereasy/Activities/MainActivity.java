f080f3c68ac5f85cf37b3a0061fcd3225a80cace
pk1409 <prashantkrishna1409@gmail.com>
2018-04-04 22:25:42 +0530

    pk- Main activity libraries imported

--- app/src/main/java/com/oeasy/ordereasy/Activities/MainActivity.java
+++ app/src/main/java/com/oeasy/ordereasy/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.oeasy.ordereasy;
+package com.oeasy.ordereasy.Activities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+        import com.oeasy.ordereasy.R;
 
 public class MainActivity extends AppCompatActivity {
 
