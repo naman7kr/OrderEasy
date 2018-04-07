982c63852b8084fc7cddcd9affa7a4d269b6dc10
KumarNaman <naman7kr@gmail.com>
2018-04-07 13:13:46 +0530

    stn-cart activity created

---
+++ app/src/main/java/com/oeasy/ordereasy/Activities/CartActivity.java
@@ -0,0 +1,18 @@
+package com.oeasy.ordereasy.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.oeasy.ordereasy.R;
+
+/**
+ * Created by Stan on 4/6/2018.
+ */
+
+public class CartActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
