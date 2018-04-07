fc84be360c0e5366e0df9d811e0040a720987cf7
KumarNaman <naman7kr@gmail.com>
2018-04-07 13:20:07 +0530

    stn-on back press functionality added

--- app/src/main/java/com/oeasy/ordereasy/Activities/ScannerActivity.java
+++ app/src/main/java/com/oeasy/ordereasy/Activities/ScannerActivity.java
@@ -1,7 +1,9 @@
 package com.oeasy.ordereasy.Activities;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -10,9 +12,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
+import com.oeasy.ordereasy.R;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -21,36 +25,22 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
  * Created by Stan on 4/5/2018.
  */
 
-public class
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+public class ScannerActivity extends BaseActivity implements ZXingScannerView.ResultHandler {
     private static final int REQUEST_CAMERA=1;
     private ZXingScannerView sView;
+    ZXingScannerView zscanner;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+
+        setToolbar();
+
+
+        zscanner = (ZXingScannerView) findViewById(R.id.scanner_zxing);
         sView=new ZXingScannerView(this);
-        setContentView(sView);
+        zscanner.addView(sView);
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
             if(checkPermission()){
                 Toast.makeText(this,"Permission is granted",Toast.LENGTH_LONG).show();
@@ -59,11 +49,63 @@ ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHand
                 requestCameraPermission();
             }
         }
-
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
+    private void setToolbar() {
+        toolbar=getToolbar();
+        getSupportActionBar().setTitle("QR Scanner");
+        toolbar.setNavigationIcon(R.drawable.ic_action_arrowback);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+    }
+
     private void requestCameraPermission() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA))
+                        Toast.makeText(this, "No Permission to use the Camera services", Toast.LENGTH_SHORT).show();
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+                }
+
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay!
+
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        finish();
+                    }
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
     private boolean checkPermission(){
@@ -74,6 +116,12 @@ ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHand
     public void handleResult(Result result) {
         String sResult=result.getText();
         Log.e("QR",sResult);
+        if(sResult.compareToIgnoreCase("Stan")==0)
+             startActivity(new Intent(this,CartActivity.class));
+        else {
+            Toast.makeText(this,"QR not accepted",Toast.LENGTH_LONG).show();
+            sView.resumeCameraPreview(this);
+        }
     }
 
     @Override
@@ -99,6 +147,7 @@ ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHand
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if(checkPermission())
         sView.stopCamera();
     }
 }
