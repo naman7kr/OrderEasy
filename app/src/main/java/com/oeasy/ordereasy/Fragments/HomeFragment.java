f78e1993d96c47dc541d18a96644d84a9bfeca50
KumarNaman <naman7kr@gmail.com>
2018-04-08 22:46:04 +0530

    stn-volley wrk started

--- app/src/main/java/com/oeasy/ordereasy/Fragments/HomeFragment.java
+++ app/src/main/java/com/oeasy/ordereasy/Fragments/HomeFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -14,13 +15,25 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.oeasy.ordereasy.Activities.MenuActivity;
 import com.oeasy.ordereasy.Adapters.HomeRecyclerAdapter;
 import com.oeasy.ordereasy.Modals.FoodItem;
 import com.oeasy.ordereasy.Others.Constants;
+import com.oeasy.ordereasy.Others.RequestHandler;
 import com.oeasy.ordereasy.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Stan on 4/4/2018.
@@ -45,14 +58,51 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragemnt, container, false);
         initialize(view);
+
         setSlider(view);
         setRecomended(view);
         setStarters(view);
         setMainCourse(view);
         setDesserts(view);
         setDrinks(view);
-
+        getData();
         return view;
+    }
+    public void getData() {
+        StringRequest request=new StringRequest(Request.Method.POST, Constants.URL_TEST, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("JSON","OK");
+                try {
+
+                    JSONObject jsonObject=new JSONObject(response);
+
+                    for(int i=0;i<jsonObject.length();i++){
+//                        Log.e("JSON",jsonObject.get
+//                        Log.e("JSON1",jsonArray.getJSONObject(i).getString("id"));
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(),error.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params= new HashMap<>();
+                params.put("four", "s");
+                return params;
+            }
+        };
+        //RequestQueue requestQueue= Volley.newRequestQueue(this);
+        //requestQueue.add(request);
+        RequestHandler.getInstance(getContext()).addToRequestQueue(request);
     }
     private void initialize(View view) {
 
