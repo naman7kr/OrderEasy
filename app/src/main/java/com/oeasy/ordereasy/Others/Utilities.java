9b27474470bba760983d970d3553f3c64328e821
KumarNaman <naman7kr@gmail.com>
2018-04-08 22:49:24 +0530

    stn-hmm

--- app/src/main/java/com/oeasy/ordereasy/Others/Utilities.java
+++ app/src/main/java/com/oeasy/ordereasy/Others/Utilities.java
@@ -1,6 +1,7 @@
 package com.oeasy.ordereasy.Others;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
@@ -10,7 +11,9 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.oeasy.ordereasy.Modals.FoodItem;
 import com.oeasy.ordereasy.R;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
@@ -28,12 +31,11 @@ public class Utilities {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
+}
     }
 
     public static void setPicassoImage(final Context context, final String imgSrc, final ImageView iv, final int FLAG){
         if (FLAG == Constants.SQUA_PLACEHOLDER){
-            Log.e("HM","HM");
             Picasso.with(context).load(imgSrc).placeholder(R.drawable.placeholder_sqaure).fit().networkPolicy(NetworkPolicy.OFFLINE).into(iv, new Callback() {
                 @Override
                 public void onSuccess() {
@@ -61,4 +63,5 @@ public class Utilities {
     public static void setPageTransitionAnimation(ViewPager mPager) {
         mPager.setPageTransformer(true,new FlipHorizontalTransformer());
     }
+
 }
